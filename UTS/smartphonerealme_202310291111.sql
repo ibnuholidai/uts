7eb9cf56855960630a38d02e6deb1803dc1da239
ibnuholidai <59520807+ibnuholidai@users.noreply.github.com>
2023-10-29 11:56:32 +0700

    Add files via upload

---
+++ UTS/smartphonerealme_202310291111.sql
@@ -0,0 +1,11 @@
+INSERT INTO smartphonerealme (brand,ram,prosesor,"storage",baterai,harga,ukuran_layar) VALUES
+	 ('Realme C11','2 GB','MediaTek Helio G35','32 GB','5.000 mAh','Rp.1.499.000','6.5 inch'),
+	 ('Realme C15','4 GB','MediaTek Helio G35','64 GB','6.000 mAh','Rp.1.999.000','6.5 inch'),
+	 ('Realme 8 Pro','8 GB','Qualcomm Snapdragon 720G','128 GB','4.500 mAh','Rp.3.999.000','6.4 inch'),
+	 ('Realme X7 Pro','8 GB','MediaTek Dimensity 1000+','128 GB','4.500 mAh','Rp.4.999.000','6.55 inch'),
+	 ('Realme GT Neo','8 GB','MediaTek Dimensity 1200','128 GB','4.500 mAh','Rp.4.999.000','6.43 inch'),
+	 ('Realme X50 Pro','12 GB','Qualcomm Snapdragon 865','128 GB','4.200 mAh','Rp.7.999.000','6.44 inch'),
+	 ('Realme Narzo 50A','4 GB','MediaTek Helio G85','64 GB','6.000 mAh','Rp.2.499.000','6.5 inch'),
+	 ('Realme 9','8 GB','MediaTek Dimensity 810','128 GB','5.000 mAh','Rp.3.999.000','6.5 inch'),
+	 ('Realme X9 Pro','12 GB','Qualcomm Snapdragon 870','128 GB','4.500 mAh','Rp.5.999.000','6.55 inch'),
+	 ('Realme GT 2','12 GB','Qualcomm Snapdragon 898','128 GB','5.000 mAh','Rp.5.970.000','6.62 inch');
